98cd39b130f5633a16660f2a7536c4ae1b88817d
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-26 11:52:11 +0300

    created card loader, and 404 button + responsice

--- src/Components/Layout/Main/PageNotFound/PageNotFound.tsx
+++ src/Components/Layout/Main/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,15 @@
 import "./PageNotFound.css";
 import img from "../PageNotFound/404.jpeg"
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound(): JSX.Element {
+    const navigate = useNavigate();
     return (
         <div className="PageNotFound">
 			<div><h1> Page NOT Found</h1></div>
+            <button className="button-17" onClick={() => navigate("/")} role="button">Back Home</button>
 			<div><img src={img} alt="" /></div>
+
         </div>
     );
 }
